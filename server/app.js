2bf3dc782f25a8a7ca1272bfe58d23442743f09c
Aryse Tansy <medictansy@gmail.com>
2023-02-06 13:39:04 -0500

    Fix: Add deployed whitelist cors

--- server/app.js
+++ server/app.js
@@ -44,7 +44,11 @@ process.env.NODE_ENV === "development" && app.use(morgan("dev"));
 app.use(express.json({ limit: "10kb" }));
 app.use(cookieParser());
 app.use(
-  cors({ origin: "http://localhost:5173", credentials: true, methods: ["GET"] })
+  cors({
+    origin: ["http://localhost:5173", "https://m11-n5ne.onrender.com"],
+    credentials: true,
+    methods: ["GET", "POST", "PATCH", "DELETE", "PUT"],
+  })
 );
 app.use(mongoSanitize());
 app.use(xss());
