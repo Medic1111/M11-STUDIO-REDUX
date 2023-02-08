59176eb505bc3a15efc5e711eaf19802d8e7a1ac
Aryse Tansy <medictansy@gmail.com>
2023-02-08 07:58:56 -0500

    Refactor: API Slice split concern

--- client/src/features/api-slice.jsx
+++ client/src/features/api-slice.jsx
@@ -3,19 +3,19 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 export const authApi = createApi({
   reducerPath: "AUTH_API",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:3002/api/v1",
+    baseUrl: "http://localhost:3002/api/v1/auth",
     credentials: "include",
   }),
   endpoints: (builder) => ({
     validate: builder.query({
-      query: () => "/auth/validate",
+      query: () => "/validate",
     }),
     logout: builder.query({
-      query: () => "/auth/logout",
+      query: () => "/logout",
     }),
     login: builder.mutation({
       query: (formData) => ({
-        url: "/auth/login",
+        url: "/login",
         method: "POST",
         body: formData,
       }),
@@ -27,23 +27,33 @@ export const authApi = createApi({
         body: formData,
       }),
     }),
+  }),
+});
+
+export const cartApi = createApi({
+  reducerPath: "CART_API",
+  baseQuery: fetchBaseQuery({
+    baseUrl: "http://localhost:3002/api/v1/user",
+    credentials: "include",
+  }),
+  endpoints: (builder) => ({
     removeFromCart: builder.mutation({
       query: (args) => ({
-        url: `/user/${args.userId}/cart`,
+        url: `/${args.userId}/cart`,
         method: "PUT",
         body: { product: args.itemId, quantity: args.quantity },
       }),
     }),
     increaseQty: builder.mutation({
       query: (args) => ({
-        url: `/user/${args.userId}/cart`,
+        url: `/${args.userId}/cart`,
         method: "POST",
         body: { product: args.itemId },
       }),
     }),
     decreaseQty: builder.mutation({
       query: (args) => ({
-        url: `/user/${args.userId}/cart`,
+        url: `/${args.userId}/cart`,
         method: "PATCH",
         body: { product: args.itemId },
       }),
@@ -51,49 +61,15 @@ export const authApi = createApi({
   }),
 });
 
-// export const cartApi = createApi({
-//   reducerPath: "CART_API",
-//   baseQuery: fetchBaseQuery({
-//     baseUrl: "http://localhost:3002/api/v1/user",
-//     credentials: "include",
-//   }),
-//   endpoints: (builder) => ({
-//     decreaseQty: builder.mutation({
-//       query: (userId, itemId) => ({
-//         url: `/user/${userId}/cart`,
-//         method: "PATCH",
-//         body: { product: itemId },
-//       }),
-//     }),
-//     removeFromCart: builder.mutation({
-//       query: (userId, itemId, quantity) => ({
-//         url: `/${userId}/cart`,
-//         method: "PUT",
-//         body: { product: itemId, quantity },
-//       }),
-//     }),
-//     increaseQty: builder.mutation({
-//       query: (userId, itemId) => ({
-//         url: `/${userId}/cart`,
-//         method: "POST",
-//         body: { product: itemId },
-//       }),
-//     }),
-//   }),
-// });
-
 export const {
   useValidateQuery,
   useLazyLogoutQuery,
   useLoginMutation,
   useRegisterMutation,
+} = authApi;
+
+export const {
   useRemoveFromCartMutation,
   useIncreaseQtyMutation,
   useDecreaseQtyMutation,
-} = authApi;
-
-// export const {
-//   useRemoveFromCartMutation,
-//   useIncreaseQtyMutation,
-//   useDecreaseQtyMutation,
-// } = cartApi;
+} = cartApi;
--- client/src/store/store.jsx
+++ client/src/store/store.jsx
@@ -2,16 +2,20 @@ import { configureStore } from "@reduxjs/toolkit";
 import uiReducer from "../features/ui-slice";
 import authReducer from "../features/auth-slice";
 import { authApi } from "../features/api-slice";
-// import { cartApi } from "../features/api-slice";
+import { cartApi } from "../features/api-slice";
 
 const store = configureStore({
   reducer: {
     ui: uiReducer,
     auth: authReducer,
     [authApi.reducerPath]: authApi.reducer,
+    [cartApi.reducerPath]: cartApi.reducer,
   },
   middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(authApi.middleware);
+    return getDefaultMiddleware().concat([
+      authApi.middleware,
+      cartApi.middleware,
+    ]);
   },
 });
 
