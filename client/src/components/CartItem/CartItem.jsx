ce09142acb8228551a600071bcaa3eb675782a24
Aryse Tansy <medictansy@gmail.com>
2023-02-08 12:41:12 -0500

    SOLID: Break down cart comp

--- client/src/components/CartItem/CartItem.jsx
+++ client/src/components/CartItem/CartItem.jsx
@@ -1,46 +1,7 @@
 import classes from "./CartItem.module.css";
-import { useDispatch, useSelector } from "react-redux";
-import { authActions } from "../../features/auth-slice";
-import { uiActions } from "../../features/ui-slice";
-import {
-  useDecreaseQtyMutation,
-  useIncreaseQtyMutation,
-  useRemoveFromCartMutation,
-} from "../../features/api-slice";
+import CartCrud from "../CartCrud/CartCrud";
 
 const CartItem = ({ obj, quantity }) => {
-  const dispatch = useDispatch();
-  const userId = useSelector((state) => state.auth.currentUser._id);
-  const isAuth = useSelector((state) => state.auth.isAuth);
-
-  const [remove] = useRemoveFromCartMutation();
-  const [increase] = useIncreaseQtyMutation();
-  const [decrease] = useDecreaseQtyMutation();
-
-  const removeHandler = async (itemId) => {
-    if (!isAuth) return dispatch(uiActions.setShowAuth(true));
-    let args = { userId, itemId, quantity };
-    let response = await remove(args);
-    if (!response.error)
-      return dispatch(authActions.setCurrentUser(response.data));
-  };
-
-  const increaseQty = async (itemId) => {
-    if (!isAuth) return dispatch(uiActions.setShowAuth(true));
-    if (obj.stock < 1) return;
-    let args = { userId, itemId };
-    let response = await increase(args);
-    if (!response.error)
-      return dispatch(authActions.setCurrentUser(response.data));
-  };
-
-  const decreaseQty = async (itemId) => {
-    if (!isAuth) return dispatch(uiActions.setShowAuth(true));
-    let args = { userId, itemId };
-    let response = await decrease(args);
-    if (!response.error)
-      return dispatch(authActions.setCurrentUser(response.data));
-  };
   return (
     <div className={classes.card}>
       <img alt="wall/street art" className={classes.img} src={obj.url} />
@@ -49,26 +10,7 @@ const CartItem = ({ obj, quantity }) => {
         <p className={classes.pDes}>{obj.description}</p>
         <p className={classes.pArtist}>- {obj.artist}</p>
         <p className={classes.pPrice}>${obj.price}</p>
-        <div className={classes.crud}>
-          <p className={classes.qty}>
-            qty: {quantity}
-            <span
-              onClick={() => increaseQty(obj.id)}
-              className={`${classes.icon} material-symbols-outlined`}
-            >
-              arrow_upward
-            </span>
-            <span
-              onClick={() => decreaseQty(obj.id)}
-              className={`${classes.icon} material-symbols-outlined`}
-            >
-              arrow_downward
-            </span>
-          </p>
-          <p className={classes.pRemove} onClick={() => removeHandler(obj.id)}>
-            remove
-          </p>
-        </div>
+        <CartCrud objId={obj.id} quantity={quantity} objStock={obj.stock} />
       </div>
     </div>
   );
