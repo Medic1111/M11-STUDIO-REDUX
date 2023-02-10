51759037c95b003713120657fcbd2fe7078edf60
Aryse Tansy <medictansy@gmail.com>
2023-02-10 09:20:24 -0500

    Revert: Issue in baseUrl RTK

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/api": {
-        target: "https://localhost:3002",
+        target: "http://localhost:3002",
         changeOrigin: true,
         withCredentials: true,
       },
