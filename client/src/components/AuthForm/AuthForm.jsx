068c8d23855f082f29238dfb2b37b8a3d25054bf
Aryse Tansy <medictansy@gmail.com>
2023-02-19 12:43:15 -0500

    GLITCH: Mobile, attempt fix prod

--- client/src/components/AuthForm/AuthForm.jsx
+++ client/src/components/AuthForm/AuthForm.jsx
@@ -1,16 +1,30 @@
 import classes from "./AuthForm.module.css";
-import { useState } from "react";
+import React, { useState } from "react";
 import { Slide } from "react-awesome-reveal";
 import AuthUpdate from "../AuthUpdate/AuthUpdate";
 import AuthForgot from "../AuthForgot/AuthForgot";
 import AuthIn from "../AuthIn/AuthIn";
+import { useMediaQuery } from "react-responsive";
 
 const AuthForm = () => {
   const [showForgot, setShowForgot] = useState(false);
   const [showConfirm, setShowPassConfirm] = useState(false);
 
+  const notMobileView = useMediaQuery({ query: "(max-width:700px)" });
+
+  const Wrapper = ({ children }) => {
+    if (notMobileView) {
+      return <React.Fragment>{children}</React.Fragment>;
+    }
+    return (
+      <Slide className={classes.form} direction="up">
+        {children}
+      </Slide>
+    );
+  };
+
   return (
-    <Slide className={classes.form} direction="up">
+    <Wrapper>
       {!showForgot ? (
         <AuthIn setShowForgot={setShowForgot} />
       ) : (
@@ -26,7 +40,7 @@ const AuthForm = () => {
           )}
         </form>
       )}
-    </Slide>
+    </Wrapper>
   );
 };
 
